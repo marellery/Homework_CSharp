a1e296c137ebc4882505ae149a25b020d570dd46
marellery <marellery@ya.ru>
2022-08-25 09:39:42 +0500

    первая задача домашнего задания

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+//Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+Console.WriteLine("Введите первое число");
+int number_1 = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int number_2 = Convert.ToInt32(Console.ReadLine());
+
+Console.Clear;
+
+if (number_1>number_2)
+{
+ Console.WriteLine("Первое число - больше");
+}
+
+else
+{
+Console.WriteLine("Второе число больше");
+
+}
