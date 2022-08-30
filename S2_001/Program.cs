44661b57a3e9b59b6e67c90a4cb5d3a4513323ce
marellery <marellery@ya.ru>
2022-08-30 19:10:03 +0500

    первая задача второго семинара

---
+++ S2_001/Program.cs
@@ -0,0 +1,17 @@
+﻿//Задача1. Ввести трехзначное число и вывести вторую цифру
+
+
+Console.WriteLine("Введите трехначное число");
+int num =Convert.ToInt32(Console.ReadLine());
+
+int num1 = num/10%10;
+
+if (num<99 || num>999)
+{
+    Console.WriteLine("Число не из заданного промежутка");
+}
+
+else
+{
+    Console.WriteLine(num1);
+}
