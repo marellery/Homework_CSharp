cccad9975ba0e658d9f88b74baee106452564209
marellery <marellery@ya.ru>
2022-08-30 19:15:13 +0500

    Вторая задача второго семинара

---
+++ S2_002/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+Console.WriteLine("Введите трехначное число");
+int num =Convert.ToInt32(Console.ReadLine());
+int num1 = num%10;
+
+if (num<99 || num>999)
+{
+    Console.WriteLine("Число не из заданного промежутка");
+}
+
+else
+{
+    Console.WriteLine(num1);
+}
