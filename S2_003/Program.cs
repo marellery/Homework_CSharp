c67a6f9daa4fb792b2d40574599f94f75b8340bf
marellery <marellery@ya.ru>
2022-08-30 19:27:30 +0500

    третья задача второго семинара

---
+++ S2_003/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+Console.WriteLine("Введите номер дня недели");
+int num =Convert.ToInt32(Console.ReadLine());
+
+
+
+    
+if (num<1 || num>7)
+{
+    Console.WriteLine("Такого дня недели нет");
+}
+else
+{
+    if (num==6 || num==7)
+    {
+        Console.WriteLine("УРА!!ВЫХОДНОЙ!");
+    }
+    else
+    {
+        Console.WriteLine("Это будний день...");
+    }
+    
+}
